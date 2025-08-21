f44b8a8c87c49dd0f22e4f2fcca2921e3f6ce99e
LucasLiaw25 <lucasaliaw@gmail.com>
2025-08-21 14:31:44 -0300

    Primeiro commit

---
+++ src/test/java/com/liaw/BibliotechAPI/BibliotechApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liaw.BibliotechAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotechApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
