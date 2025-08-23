964b84ec6b46ee04e08fa5718569782bb6b76fc6
LucasLiaw25 <lucasaliaw@gmail.com>
2025-08-23 11:49:10 -0300

    feat: Adiciona QueryByExampleExecutor nos repositories de Book e Users

--- src/main/java/com/liaw/BibliotechAPI/repository/BookRepository.java
+++ src/main/java/com/liaw/BibliotechAPI/repository/BookRepository.java
@@ -1,4 +1,8 @@
 package com.liaw.BibliotechAPI.repository;
 
-public interface BookRepository {
+import com.liaw.BibliotechAPI.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
+
+public interface BookRepository extends JpaRepository<Book, Long>, QueryByExampleExecutor<Book> {
 }
--- src/main/java/com/liaw/BibliotechAPI/repository/UsersRepository.java
+++ src/main/java/com/liaw/BibliotechAPI/repository/UsersRepository.java
@@ -2,11 +2,9 @@ package com.liaw.BibliotechAPI.repository;
 
 import com.liaw.BibliotechAPI.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
 
 import java.util.Optional;
 
-public interface UsersRepository extends JpaRepository<Users, Long> {
-    Optional<Users> findByIdAndCpfAndName(
-            Long id, String cpf, String name
-    );
+public interface UsersRepository extends JpaRepository<Users, Long>, QueryByExampleExecutor<Users> {
 }
