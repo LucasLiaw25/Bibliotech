97935eca8904095a03b7d0cd5f813db2f71b1d8d
LucasLiaw25 <lucasaliaw@gmail.com>
2025-08-23 18:22:52 -0300

    feat: Trata exceptions de Máximo de empréstimos e livro já emprestado

--- src/main/java/com/liaw/BibliotechAPI/exception/BookBorrowedException.java
+++ src/main/java/com/liaw/BibliotechAPI/exception/BookBorrowedException.java
@@ -1,7 +1,7 @@
 package com.liaw.BibliotechAPI.exception;
 
 public class BookBorrowedException extends RuntimeException {
-  public BookBorrowedException(String message) {
-    super(message);
-  }
+    public BookBorrowedException(String message) {
+        super(message);
+    }
 }
--- src/main/java/com/liaw/BibliotechAPI/global/GlobalExceptionHandler.java
+++ src/main/java/com/liaw/BibliotechAPI/global/GlobalExceptionHandler.java
@@ -1,27 +1,66 @@
 package com.liaw.BibliotechAPI.global;
 
-import com.liaw.BibliotechAPI.exception.BookNotFoundException;
-import com.liaw.BibliotechAPI.exception.ErrorMessage;
-import com.liaw.BibliotechAPI.exception.UserNotFoundException;
+import com.liaw.BibliotechAPI.exception.*;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(UserNotFoundException.class)
     public ErrorMessage handleUserNotFoundException(UserNotFoundException e){
         return new ErrorMessage(
                 HttpStatus.NOT_FOUND.value(),
-                "Usuário não encontrado",
+                e.getMessage(),
                 List.of()
                 );
     }
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(BookNotFoundException.class)
-    public ErrorMessage handleBookNotFoundException
+    public ErrorMessage handleBookNotFoundException(BookNotFoundException e){
+        return new ErrorMessage(
+                HttpStatus.NOT_EXTENDED.value(),
+                e.getMessage(),
+                List.of()
+        );
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorMessage handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        List<FieldError> fieldErrors = e.getFieldErrors();
+        List<ErrorField> fields = fieldErrors.stream().map(fe-> new ErrorField(
+                fe.getField(), fe.getDefaultMessage()
+        )).toList();
+
+        return new ErrorMessage(HttpStatus.BAD_REQUEST.value(), "Insira um valor válido", fields);
+    }
+
+    @ExceptionHandler(MaxLoanBookException.class)
+    public ErrorMessage handleMaxLoanBookException(MaxLoanBookException e){
+        return new ErrorMessage(
+                HttpStatus.CONFLICT.value(),
+                e.getMessage(),
+                List.of()
+        );
+    }
+
+    @ExceptionHandler(BookBorrowedException.class)
+    public ErrorMessage handleBookBorrowedException(BookBorrowedException e){
+        return new ErrorMessage(
+                HttpStatus.CONFLICT.value(),
+                e.getMessage(),
+                List.of()
+        );
+    }
 
 }
